b33574136147e8cca562460970384eba6e915a7d
thefuzzyjew <asnyder@nfv.k12.ia.us>
2019-03-04 17:48:09 -0600

    Fixed Setup.sh and docker container creation now works

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #macOS and Linux setup script
 docker-compose build
-docker-compose up --no-start
-docker-compose run web python jamf_webhook_connector/manage.py createsuperuser
-docker start jamf_webhook_receiver_db_1
-docker start jamf_webhook_receiver_web_1
+docker-compose up -d
+#sleep to let migrations finish
+echo "Waiting for Migrations..."
+sleep 10
+# Create a super user
+docker exec -it $(docker ps -aqf "name=jamfhooks_web") python jamf_webhook_connector/manage.py createsuperuser
+
+#docker-compose run web python jamf_webhook_connector/manage.py createsuperuser
+#docker start jamfhooks_db
+#docker start jamfhooks_web
