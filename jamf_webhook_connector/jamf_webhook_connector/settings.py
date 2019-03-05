46ad2846b2652b364a663584adbdfc894c842c6d
thefuzzyjew <asnyder@nfv.k12.ia.us>
2019-03-05 16:07:08 -0600

    Docker build now uses a postgres container along with an nginx reverse proxy to serve content

--- jamf_webhook_connector/jamf_webhook_connector/settings.py
+++ jamf_webhook_connector/jamf_webhook_connector/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'jamf_webhook_receiver/templates/jamf_webhook_receiver')
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 MEDIA_DIR = os.path.join(BASE_DIR, "media")
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
