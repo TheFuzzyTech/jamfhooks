8370254552583386a36b39167b916ce32abf1221
thefuzzyjew <asnyder@nfv.k12.ia.us>
2019-03-04 18:04:27 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #macOS and Linux setup script
-docker-compose build
-docker-compose up -d
+docker-compose build --no-cache
+docker-compose up -d --force-recreate
 #sleep to let migrations finish
 echo "Waiting for Migrations..."
 sleep 10
 # Create a super user
 docker exec -it $(docker ps -aqf "name=jamfhooks_web") python jamf_webhook_connector/manage.py createsuperuser
-
+echo "visit 127.0.0.1:8000/admin to log in"
+echo "visit 127.0.0.1:8000/webhooks after logging in to begin"
 #docker-compose run web python jamf_webhook_connector/manage.py createsuperuser
 #docker start jamfhooks_db
 #docker start jamfhooks_web
