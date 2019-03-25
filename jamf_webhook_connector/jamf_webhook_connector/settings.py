e257600553e9f9f65bcbc838390917586bf123fe
thefuzzyjew <asnyder@nfv.k12.ia.us>
2019-03-25 16:17:01 -0500

    Created Integration Views

--- jamf_webhook_connector/jamf_webhook_connector/settings.py
+++ jamf_webhook_connector/jamf_webhook_connector/settings.py
@@ -77,12 +77,25 @@ WSGI_APPLICATION = 'jamf_webhook_connector.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'yourdatabasename.db'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('SQL_USER'),
+#         'USER': os.getenv('SQL_USER'),
+#         'PASSWORD': os.getenv('SQL_PASSWORD'),
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
