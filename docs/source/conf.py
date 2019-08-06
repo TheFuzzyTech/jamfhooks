9bda463c7b6a317ec00464a4b539d00734d5cc4c
thefuzzyjew <asnyder@nfv.k12.ia.us>
2019-08-06 12:20:40 -0500

    Read the Docs fixes I think

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Alex Snyder'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
