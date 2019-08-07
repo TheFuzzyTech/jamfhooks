f72bfcd367d7b89f168d7756537b0c75fc2ea1c6
thefuzzyjew <asnyder@nfv.k12.ia.us>
2019-08-07 11:09:50 -0500

    Added skipping account creation during travisCI

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@ docker-compose up -d --force-recreate
 echo "Waiting for Migrations..."
 sleep 10
 # Create a super user
-docker exec -it $(docker ps -aqf "name=jamfhooks_web") python jamf_webhook_connector/manage.py createsuperuser
+if [ $TESTING = true ]; then
+  echo "Testing, skip user creation"
+else
+  docker exec -it $(docker ps -aqf "name=jamfhooks_web") python manage.py createsuperuser
+fi
 echo "visit 127.0.0.1:8000/admin to log in"
 echo "visit 127.0.0.1:8000/webhooks after logging in to begin"
 #docker-compose run web python jamf_webhook_connector/manage.py createsuperuser
