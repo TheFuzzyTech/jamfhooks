3ae06eb5025d7889f1ce13b00d9483762907e704
thefuzzyjew <asnyder@nfv.k12.ia.us>
2019-08-07 10:44:27 -0500

    Fix setup.sh remove linting from manage.py

--- jamf_webhook_connector/manage.py
+++ jamf_webhook_connector/manage.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
+# fmt: off
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jamf_webhook_connector.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jamf_webhook_connector.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "forget to activate a virtual environment?") from exc
     execute_from_command_line(sys.argv)
