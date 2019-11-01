577d5c5d21daeafe6f5433e5f13f6e9cabc2837c
Alex Snyder <asnyder@nfv.k12.ia.us>
2019-11-01 11:09:44 -0500

    1.0.1
    Closes #4 , #5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,24 +17,23 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'JamfHooks'
-copyright = '2019, Alex Snyder'
-author = 'Alex Snyder'
+project = "JamfHooks"
+copyright = "2019, Alex Snyder"
+author = "Alex Snyder"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
-master_doc = 'index'
+release = "1.0"
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,9 +46,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
